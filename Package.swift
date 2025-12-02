26374c2c40013f9e352f37469a23f22a0b11b858
Leinnan <siuszko@zoho.com>
2025-12-02 20:11:50 +0000

    Build: (ed14424) Update to new bevy_ads_common

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "bevy_ads_admob_ios",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "bevy_ads_admob_ios",
+            targets: ["bevy_ads_admob_ios"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            from: "12.14.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AdmobXcframework",
+            path: "AdmobXcframework.xcframework"),
+        .target(
+            name: "bevy_ads_admob_ios",
+            dependencies: [
+                "AdmobXcframework",
+                .product(
+                    name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+            ],
+        ),
+    ]
+)
